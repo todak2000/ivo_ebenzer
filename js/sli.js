a828d5d10572f00f8f871b622ab72a9c6e60809f
Daniel Olagunju <todak2000@gmail.com>
2018-11-05 18:51:33 +0100

    initial commit222

---
+++ js/sli.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.my-class').slick({
+        dots: true,
+        infinite: true,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 2000
+      });
+  });
+          
+ 
